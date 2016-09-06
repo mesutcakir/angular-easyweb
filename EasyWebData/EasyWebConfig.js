e8b473f2206b5ec4b4f8bbc6909ce24468a76cbf
mesut.cakir <mesut.cakir@argelog.com.tr>
2016-09-06 14:47:27 +0300

    proje yüklendi.

---
+++ EasyWebData/EasyWebConfig.js
@@ -0,0 +1,14 @@
+﻿angular.module('EasyWeb',["ngSanitize"])
+.controller("EasyWebController",["$scope","$http",
+function($scope,$http){
+  $http.get("EasyWebData/Index.json").success(function(data){
+        $scope.Model = data;
+    });
+}])
+
+.controller("EasyWebHeadController",["$scope","$http",
+function($scope,$http){
+  $http.get("EasyWebData/Head.json").success(function(data){
+        $scope.Model = data;
+    });
+}]);
